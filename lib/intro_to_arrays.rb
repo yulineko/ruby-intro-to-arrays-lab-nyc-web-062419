a3488cbb93f1a90397288c1da15c1b8d158532da
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-25 03:18:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,7 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  array.first
+  array[0]
 end  
 
 def third_element(array)
@@ -17,4 +17,9 @@ end
 def last_element(array)
   array[-1]
 end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
 
